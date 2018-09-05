7671a0ff14fa8adc28e8d8df52005aea6c3592e8
Martijn Vos <mcvmcv@xs4all.nl>
2018-09-05 23:06:00 +0200

    cleanup logging

--- site/gdl.js
+++ site/gdl.js
@@ -6,7 +6,7 @@ class GDL {
         this.eventsToProcess = [];
         this.publishedEvents = [];
         this.requestedPayloadDefinitions = [];
-        console.log("constructor");
+        this.unknownEvents = [];
         this.fetchEventDefs();
     }
     /**
@@ -18,7 +18,6 @@ class GDL {
      * @return void
      */
     subscribe (category, name, handler) {
-        console.log("subscribe");
         let eventHandler = new EventHandler(category, name, handler);
         if (this.handlers) {
             // if handlers is ready to receive handlers, subscribe event handler
@@ -37,7 +36,6 @@ class GDL {
     }
 
     registerEventHandlers () {
-        console.log("subscribe events: ", this.handlersToProcess);
         this.handlersToProcess.forEach(handler => this.registerEventHandler(handler));
     }
 
@@ -52,6 +50,7 @@ class GDL {
      * @return void
      */
     publish (category, name, payload) {
+        console.log("publish: ",category, name, payload);
         let event = new GdlEvent(category, name, payload);
         this.publishedEvents.push(event);
         if (this.handlers) {
@@ -60,30 +59,33 @@ class GDL {
     };
 
     publishGdlEvent(event) {
-        console.log("publish directly", this.handlers[event.category][event.name].hander);
         if (this.handlers[event.category] && this.handlers[event.category][event.name]) {
             let handlers = this.handlers[event.category][event.name];
             handlers.forEach(handler => handler(event));
         }
         else {
             console.warn("unknown event: ",event);
+            this.unknownEvents.push(event);
         }
 
     }
 
     publishQueuedEvents() {
-        console.log("publishQueuedEvents: ", this.publishedEvents);
         this.publishedEvents.forEach(event => this.publishGdlEvent(event));
     }
 
     /**
-     * Calls async callback with payload specifications for requested event type
+     * Request the definition of the payload for a event type
+     * If event definitions are available, it calls async callback with the payload definition
+     * for the requested event type.
+     * If event definitions are not available, queue the request and call the callback once the
+     * definition is available.
      *
      * @param category {String}
      * @param name {String}
      * @param callback {Function}
      */
-    payloadSpec(category, name, callback) {
+    payloadDefinition(category, name, callback) {
         if (this.eventDefinitions) {
             this.sendPayloadDefinition(category, name, callback);
         }
@@ -93,13 +95,7 @@ class GDL {
     }
 
     sendPayloadDefinition(category, name, callback) {
-        console.log(category, name);
-        console.log("eventDefinitions: ",this.eventDefinitions);
         let definition = this.eventDefinitions.find(def => def.category === category && def.name === name);
-        this.eventDefinitions.forEach(def => {
-            console.log(def.category, def.name, def.category === category && def.name === name);
-        })
-        console.log("found definition: ",definition);
         callback(definition);
     }
 
@@ -110,7 +106,6 @@ class GDL {
     }
 
     fetchEventDefs() {
-        console.log("this: ",this);
         fetch("../defs/eventdefinitions.json", { headers: { "Content-Type": "application/json; charset=utf-8" }})
             .then(res => res.json())
             .then(res => this.processEventDefinitions(res))
@@ -121,10 +116,7 @@ class GDL {
     }
 
     processEventDefinitions(response) {
-        console.log("response: ", response);
-        console.log("this2: ",this);
         this.eventDefinitions = response;
-        console.log("defs: ",this.eventDefinitions);
         this.initiateHandlers(response);
         this.registerEventHandlers();
         this.publishQueuedEvents();
@@ -133,10 +125,7 @@ class GDL {
 
     initiateHandlers(eventDefs) {
         this.handlers = {};
-        console.log("defs2: ",this.eventDefinitions);
-        console.log("defs3: ",eventDefs);
         eventDefs.forEach (def => {
-            console.log("def: ",def);
             if (!this.handlers[def.category]) {
                 this.handlers[def.category] = {};
             }
