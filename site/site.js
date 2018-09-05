ff767503d92a08e2429e34c83ce99c4bc5a13530
Martijn Vos <mcvmcv@xs4all.nl>
2018-09-05 23:05:45 +0200

    send events on link clicks

--- site/site.js
+++ site/site.js
@@ -2,30 +2,6 @@
   // this is the simplest mockup-of our demo-site we could think of - please spend a bit more time on structuring your 
   // assessment code then we did in here ;)
 
-  /*
-   * preformatter for payload data to meet requirement by GDL specs
-   */
-  function formFields(form) {
-    result = {};
-    console.log(form.elements);
-    form.elements.forEach(field => {
-      result[field.name] = field.value;
-    });
-    console.log("formfields: ",result);
-  }
-
-  // const formCategoryMap = {
-  //   "signup-step1": "sign-up",
-  //   "ship-now-step1": "ship-now"
-  // };
-  //
-  // const payloadMapping = {
-  //   "sign-up": {
-  //     "form-submit": {
-  //
-  //     }
-  //   }
-  // }
   function submitForm(form) {
     const success = Math.random() < .6;
     const resultElement = form.querySelector(success ? '.thanks' : '.error');
@@ -38,9 +14,10 @@
 
     let eventname = success? "form-submit": "form-submit-error";
 
-    gdl.payloadSpec(category, eventname, spec => {
+    // form payload depends on the form. we retrieve the eventdefinition to find the proper payload
+    gdl.payloadDefinition(category, eventname, def => {
       let payload = {};
-      for (let key in spec.payload) {
+      for (let key in def.payload) {
         if (key === "formName") payload.formName = form.name;
         else payload[key] = form.elements[key].value;
       };
@@ -53,15 +30,24 @@
     d.querySelectorAll('section').forEach((section) => {
       section.className = section.id === pageId ? 'active' : '';
     });
-    gdl.publish("pageview", "pageview", pageId);
   }
 
+  let page = null;
+
   function handleRoute(url) {
+    gdl.publish("pageview", "pageview", url);
     const [ , pageId ] = /#\/(.+)$/.exec(url) || [];
     if (pageId) {
+      page = pageId;
       navigateTo(pageId);
     }
   }
+
+  d.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', event => {
+      gdl.publish(page, "click-link", {linkName: link.name? link.name: link.href});
+    })
+  });
 
   d.querySelectorAll('form').forEach((form) => {
     form.addEventListener('submit', (event) => {
@@ -69,6 +55,7 @@
       submitForm(form);
     });
   });
+
 
   window.addEventListener('popstate', () => {
     handleRoute(document.location.href);
